575c33709503e76df3306328261a1d28fbb700a5
Black <53127242+Aubert-co@users.noreply.github.com>
2021-12-10 23:26:08 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-const express = require('express')
+const express = require('express');
 const app = express()
 const cors = require('cors')
+const route = require('./route/index')
 
-
-app.use(cors)
+app.use(cors())
 app.use(express.json())
 
+app.use(route)
 
-app.listen(8080)
+app.listen(8080,()=>{
+    console.info('running at port 8080')
+})
