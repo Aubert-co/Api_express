81a490b9488dd568650c14bd89e054325fdd6dc6
Black <53127242+Aubert-co@users.noreply.github.com>
2021-12-12 19:16:47 -0300

    Update ApiDelete.js

--- controller/ApiDelete.js
+++ controller/ApiDelete.js
@@ -5,7 +5,7 @@ module.exports = async function ApiDelete(req,res){
 
         if(!id || typeof id !== 'number')return res.status(401).send({msg:'id needs to be a number'})
         const DeleteTable = await Schema.destroy({where:id})
-        res.status(200).send({msg:'sucess'})
+        res.status(200).send({msg:'successful'})
     }catch(err){
         return res.status(501).send({msg:'something went wrong'})
     }
