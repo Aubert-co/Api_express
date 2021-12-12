289e5949ee5d5d9a4fe70cfec6d7e3ae195d11df
Black <53127242+Aubert-co@users.noreply.github.com>
2021-12-11 23:26:27 -0300

    Update ApiAdd.test.js

---
+++ Test/ApiAdd.test.js
@@ -0,0 +1,44 @@
+const {expect} = require('chai');
+const request = require('supertest');
+const server = require('../server');
+
+var app;
+
+
+describe("api /apiadditem",()=>{
+    beforeAll(async()=>{
+        app = await server.listen(8081)
+    })
+    describe("test cases errors ",()=>{
+        test("should return a error when send undefined name",async()=>{
+            const response = await request(app)
+            .post('/apiadditem')
+            .set({'Content-Type':'application/json'})
+            .send({name:'',price:2.32})
+            
+            expect(response.body.msg).to.be.equal('invalid datas')
+            expect(response.status).to.be.equal(401)
+        })
+        test("should return a error when send a undefined price",async()=>{
+            const response = await request(app)
+            .post('/apiadditem')
+            .set({'Content-Type':'application/json'})
+            .send({name:'mango',price:'e2.32'})
+
+            expect(response.body.msg).to.be.equal('invalid datas')
+            expect(response.status).to.be.equal(401)
+            
+        })
+
+        test("should return a error when send a invalid name and a invalid price",async()=>{
+            const response = await request(app)
+            .post('/apiadditem')
+            .set({'Content-Type':'application/json'})
+            .send({name:5,price:'e'})
+
+            expect(response.body.msg).to.be.equal('invalid datas')
+            expect(response.status).to.be.equal(401)
+        })
+    })
+    
+})
