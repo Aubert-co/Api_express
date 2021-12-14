f069c0bcc47ed8b5d3010e503ef2f8d48d172eb9
Black <53127242+Aubert-co@users.noreply.github.com>
2021-12-13 22:09:37 -0300

    Update ApiDelete.test.js

--- Test/ApiDelete.test.js
+++ Test/ApiDelete.test.js
@@ -5,10 +5,41 @@ const server = require('../server');
 var app;
 
 
-describe("api /apiadditem",()=>{
+describe("api /apidelete",()=>{
     beforeAll(async()=>{
         app = await server.listen(8082)
     })
 
-    test("")
+    test("test cases errors",async()=>{
+        test("should return a error when send a id undefined",async()=>{
+            const response = await request(app)
+            .post('/apidelete')
+            .set({'Content-type':'application/json'})
+            .send({id:''})
+
+            expect(response.body.msg).to.be.equal('invalid id')
+            expect(response.status).to.be.equal(401)
+        })
+        test("should return a error when send a id equal a string",async()=>{
+            const response = await request(app)
+            .post('/apidelete')
+            .set({'Content-type':'application/json'})
+            .send({id:'and'})
+
+            expect(response.body.msg).to.be.equal('invalid id')
+            expect(response.status).to.be.equal(401)
+        })
+    })
+
+    test("test cases sucessful",async()=>{
+        test("should return a error when send a id undefined",async()=>{
+            const response = await request(app)
+            .post('/apidelete')
+            .set({'Content-type':'application/json'})
+            .send({id:30})
+
+            expect(response.body.msg).to.be.equal('successful')
+            expect(response.status).to.be.equal(200)
+        })
+    })
 })
